46dd162d4deb8e8dc5a80de27c4339a78dfe36ba
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-12-25 19:00:06 -0300

    feat: add Cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,8 +1,58 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootReducer } from "../../store";
+import {
+  AddRemoveButton,
+  CartContainer,
+  ProductCart,
+  ProductCartInfo,
+  ProductDetails,
+  RemoveButton,
+} from "./styles";
+import { add, remove } from "../../store/reducers/cart";
+
 const Cart = () => {
+  const items = useSelector((state: RootReducer) => state.cart.itens);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <CartContainer>
+      {items.map((item) => (
+        <ProductCart key={item.id}>
+          <ProductCartInfo>
+            <img src={item.cover} alt={item.title} />
+            <ProductDetails>
+              <h3>{item.title}</h3>
+              <span>
+                Preço:{" "}
+                {item.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+              <span>
+                Quantidade:{" "}
+                <AddRemoveButton
+                  action="remove"
+                  onClick={() => dispatch(remove(item))}
+                >
+                  -
+                </AddRemoveButton>{" "}
+                {item.amount}{" "}
+                <AddRemoveButton
+                  action="add"
+                  onClick={() => dispatch(add(item))}
+                >
+                  +
+                </AddRemoveButton>
+              </span>
+            </ProductDetails>
+          </ProductCartInfo>
+          <RemoveButton onClick={() => dispatch(remove(item))}>
+            Remover
+          </RemoveButton>
+        </ProductCart>
+      ))}
+    </CartContainer>
   );
 };
 
---
+++ src/pages/Cart/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Container } from "../../styles";
+
+type Props = {
+  action: "add" | "remove";
+};
+
+export const CartContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const ProductCart = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProductCartInfo = styled.div`
+  display: flex;
+
+  img {
+    max-width: 100%;
+    height: 150px;
+    border-radius: 3px;
+  }
+`;
+
+export const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto 0;
+  margin-left: 10px;
+  gap: 8px;
+`;
+
+export const RemoveButton = styled.button`
+  border: none;
+  background-color: red;
+  cursor: pointer;
+  padding: 8px;
+  border-radius: 6px;
+  color: #fff;
+  font-weight: bold;
+`;
+
+export const AddRemoveButton = styled.button<Props>`
+  border: none;
+  background-color: ${(props) => (props.action === "add" ? "green" : "red")};
+  padding: 0 4px;
+  color: #fff;
+  border-radius: 6px;
+  cursor: pointer;
+`;
