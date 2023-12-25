7cd65ce4d09f39cfb3e5cf1fe247c8e6a1111184
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-12-25 18:12:50 -0300

    feat: show product items quantity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import { FaShoppingCart } from "react-icons/fa";
 
-import { Logo, Header as StyledHeader } from "./styles";
+import { CartIcon, Logo, Header as StyledHeader } from "./styles";
 import { Container } from "../../styles";
+import { useSelector } from "react-redux";
+import { RootReducer } from "../../store";
 
 const Header = () => {
+  const items = useSelector((state: RootReducer) => state.cart.itens);
+
   return (
     <Container>
       <StyledHeader>
         <Logo>
           <span>Pet</span>shop
         </Logo>
-        <FaShoppingCart size={25} color={"#212121"} />
+        <CartIcon>
+          <FaShoppingCart size={25} color={"#212121"} />
+          {items.length > 0 && <div>{items.length}</div>}
+        </CartIcon>
       </StyledHeader>
     </Container>
   );
